4021815cc1b21e768fed70f6398de8d89c138ae3
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2022-10-25 23:22:31 -0300

    Second Commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import fastify from "fastify";
 const app = fastify();
 
-app.get("/", (req, res) => {
-  return res.send({ status: "ok" });
+app.get("/", (_, res) => {
+  return res
+    .status(200)
+    .send({ status: "Online" });
 })
 
 app.listen({
   port: 8080,
-})
+}, () => console.log("Saphire's API Connected"))
