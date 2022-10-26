b1b475ba7d7d2f408a2403858408b03291c5b170
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2022-10-26 11:05:30 -0300

    just a check at authorization

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import fastifyWebSocket from "@fastify/websocket";
 import fastify from "fastify";
+import { env } from "process";
 import { topggWebhook } from "./services";
 
 const app = fastify();
-
 app.register(fastifyWebSocket);
+
+app.post("/topgg", (req, res) => {
+
+  if (req.headers.authorization !== env.TOP_GG_AUTHORIZATION)
+    return res.status(401);
+
+  topggWebhook.listener(vote => console.log(`TopGG - ${vote}`));
+  return res.status(200);
+});
 
 app.get("/ws", { websocket: true }, (connection) => {
   console.log("Linha 10");
@@ -15,12 +24,12 @@ app.get("/ws", { websocket: true }, (connection) => {
   });
 });
 
-app.get("/topgg", { websocket: true }, (connection) => {
-  topggWebhook.listener((vote) => {
-    console.log(`TopGG - ${vote}`);
-    connection.socket.send(vote);
-  });
-});
+// app.get("/topgg", { websocket: true }, (connection) => {
+//   topggWebhook.listener((vote) => {
+//     console.log(`TopGG - ${vote}`);
+//     connection.socket.send(vote);
+//   });
+// });
 
 app.get("/", (_, res) => {
   return res.status(200).send({ status: "Online" });
@@ -33,4 +42,7 @@ app.get("/saphire", (_, res) => {
 app.listen({
   port: 8080,
   host: "0.0.0.0"
-}, (err, address) => console.log(200, err ?? address));
+}, (err) => err
+  ? console.log(err)
+  : console.log("Saphire's API Connected")
+);
