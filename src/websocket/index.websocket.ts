9f9e266ebccae844d5d0e67354d6c5083e935b28
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2023-07-06 00:34:15 -0300

    ok

--- src/websocket/index.websocket.ts
+++
@@ -1,8 +0,0 @@
-import { wss } from "../server";
-import socket from "./socket.websocket";
-
-wss.once("listening", () => console.log("Websocket System Started"))
-
-wss.on("connection", (WebSocket, request) => socket(WebSocket, request));
-
-wss.on("error", error => console.log(error))
