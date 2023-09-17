a1519b0a86d94f263a719881d54ace41c3067f0e
Rodrig Couto <rodrigo.couto.santos@hotmail.com>
2023-09-17 11:56:16 -0300

    ok

--- src/database/functions/load.cache.ts
+++ src/database/functions/load.cache.ts
@@ -1,18 +1,18 @@
 import TwitchManager from "../../twitch/manager.twitch";
-import { guilds, users } from "../../websocket/cache/get.cache";
-import Database from "../index";
+// import { guilds, users } from "../../websocket/cache/get.cache";
+// import Database from "../index";
 
 export default async () => {
     TwitchManager.getToken()
 
-    const guildsData = await Database.Guild.find()
-    for (const data of guildsData)
-        if (data.id) guilds.set(data.id, data.toObject())
+    // const guildsData = await Database.Guild.find()
+    // for (const data of guildsData)
+    //     if (data.id) guilds.set(data.id, data.toObject())
 
-    const usersData = await Database.User.find()
-    for (const user of usersData)
-        if (user.id) users.set(user.id, user.toObject())
+    // const usersData = await Database.User.find()
+    // for (const user of usersData)
+    //     if (user.id) users.set(user.id, user.toObject())
 
-    console.log(`${guilds.size} Guilds and ${users.size} Users Cached`)
+    // console.log(`${guilds.size} Guilds and ${users.size} Users Cached`)
     return 
 }
--- src/websocket/cache/get.cache.ts
+++ src/websocket/cache/get.cache.ts
@@ -2,15 +2,33 @@ import { Collection } from "discord.js";
 import { CallbackType } from "../../@types";
 import { GuildSchema } from "../../database/model/guilds";
 import { UserSchema } from "../../database/model/user";
+import database from "../../database";
 export const users = new Collection<string, UserSchema>();
 export const guilds = new Collection<string, GuildSchema>();
 
-export default (id: string | undefined, type: "user" | "guild" | undefined, callback: CallbackType): void => {
+export default async (id: string | undefined, type: "user" | "guild" | undefined, callback: CallbackType): Promise<void> => {
 
     if (!id || !type) return callback(null)
 
-    if (type == "guild") return callback(guilds.get(id))
-    if (type == "user") return callback(users.get(id))
+    if (type == "guild") {
+        const data = guilds.get(id)
+        if (data) return callback(data)
+
+        const guildData = await database.Guild.findOne({ id }).then(doc => doc?.toObject())
+        if (guildData) guilds.set(id, guildData)
+
+        return callback(guildData)
+    }
+
+    if (type == "user") {
+        const data = users.get(id)
+        if (data) return callback(data)
+
+        const userData = await database.User.findOne({ id }).then(doc => doc?.toObject())
+        if (userData) users.set(id, userData)
+
+        return callback(userData)
+    }
 
     return callback(null)
 }
