a3d870a061607ff5332f331277ff350d8a19a0f1
Rodrig Couto <rodrigo.couto.santos@hotmail.com>
2023-11-26 13:52:19 -0300

    ok

--- src/site/status.get.ts
+++ src/site/status.get.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
-import { shardsAndSockets } from "../websocket/connection"
+import { shards } from "../websocket/connection"
 import { Request, Response } from "express"
+import { redis, RedisRanking, RedisUsers } from "../database"
 
 export default async (_: Request, res: Response) => {
 
@@ -13,8 +14,19 @@ export default async (_: Request, res: Response) => {
         fetch("https://discord.com/api/v10/users/@me", { method: "GET", headers: { authorization: process.env.DISCORD_TOKEN } }).then(() => ms.push({ name: "discord", ms: calculate() })).catch(() => ms.push({ name: "discord", ms: 0 })),
         fetch("https://top.gg/api/bots/912509487984812043", { headers: { authorization: process.env.TOP_GG_TOKEN }, method: "GET" }).then(() => ms.push({ name: "topgg", ms: calculate() })).catch(() => ms.push({ name: "topgg", ms: 0 })),
         mongoose.connection.db.admin().ping().then(() => ms.push({ name: "database", ms: calculate() })).catch(() => ms.push({ name: "database", ms: 0 })),
-        shardsAndSockets?.random()?.timeout(7000).emitWithAck("shardsping", "get").then(data => ms.push({ name: "shards", data: data })).catch(() => ms.push({ name: "shards", data: [] }))
+        redis.ping().then(() => ms.push({ name: "rediscache", ms: calculate() })).catch(() => ms.push({ name: "rediscache", ms: 0 })),
+        RedisRanking.ping().then(() => ms.push({ name: "redisranking", ms: calculate() })).catch(() => ms.push({ name: "redisranking", ms: 0 })),
+        RedisUsers.ping().then(() => ms.push({ name: "redisuser", ms: calculate() })).catch(() => ms.push({ name: "redisuser", ms: 0 })),
     ])
+
+    ms.push(
+        {
+            name: "shards",
+            data: shards
+                .sort((a, b) => a.shardId - b.shardId)
+                .map((data, shardId) => ({ shard: shardId, ms: data?.ms || 0, guilds: data.guildsCount || 0 }))
+        }
+    )
 
     return res.send(ms)
 }
